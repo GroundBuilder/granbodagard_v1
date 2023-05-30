39107d1ad36f0909cd71f862aa1fff1213117092
GroundBuilder <erik_rynnes@hotmail.com>
2023-05-30 21:54:45 +0000

    Add courses view and templates

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .models import Course, Category
+
 # Create your views here.
+
+def all_courses(request):
+    """ A view to show all products, including sorting and search queries. """
+
+    courses = Course.objects.all()
+
+    context = {
+        'courses': courses,
+    }
+
+    return render(request, 'courses/courses.html', context)
--- granbodagard/urls.py
+++ granbodagard/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # Django-allauth required
     path('', include('home.urls')),
+    path('courses/', include('courses.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
