1ab5e3871492ef922e84669766cb7c270aba3589
GroundBuilder <erik_rynnes@hotmail.com>
2023-06-06 20:10:44 +0000

    Searsch querries works

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Course, Category
 
 # Create your views here.
@@ -8,18 +9,49 @@ from .models import Course, Category
 def all_courses(request):
     """ A view to show all courses, including sorting and search queries. """
 
+    courses = Course.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                courses = courses.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            courses = courses.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            courses = courses.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search critera!")
-                return redirect(reverse('products'))
+                return redirect(reverse('courses'))
 
-    courses = Course.objects.all()
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            courses = courses.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'courses': courses,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'courses/courses.html', context)
