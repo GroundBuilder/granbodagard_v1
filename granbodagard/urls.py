8ea9b279ea0d0d9b5d568fa0ed4fe25a3467cfe0
GroundBuilder <erik_rynnes@hotmail.com>
2023-06-12 09:07:47 +0000

    Add 404 page.

--- granbodagard/urls.py
+++ granbodagard/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('courses/', include('courses.urls')),
     path('shoppingbag/', include('shoppingbag.urls')),
+    path('shoppingbag/', include('shoppingbag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
