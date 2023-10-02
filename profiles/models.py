f3fabf52b18eb3f7680f79921aed92f4f2bb4fac
GroundBuilder <erik_rynnes@hotmail.com>
2023-10-02 19:53:21 +0000

    Add function to app profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    """
+    Use to store order history for user.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
