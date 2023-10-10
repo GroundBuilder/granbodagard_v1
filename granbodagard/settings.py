e85c356807ac7dbe02b3e11d5d085fea6f9a9318
GroundBuilder <erik_rynnes@hotmail.com>
2023-10-10 07:27:45 +0000

    Temporary hide DATABASE in settings.py

--- granbodagard/settings.py
+++ granbodagard/settings.py
@@ -125,7 +125,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'    # Brings me back to home page
+LOGOUT_REDIRECT_URL = '/'   # Brings me back to home page
 
 WSGI_APPLICATION = 'granbodagard.wsgi.application'
 
@@ -133,17 +134,17 @@ WSGI_APPLICATION = 'granbodagard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
