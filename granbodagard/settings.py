6dd1cd948fde8c832f1af5485a5ec4e6174e6ed9
GroundBuilder <erik_rynnes@hotmail.com>
2023-11-05 21:09:41 +0000

    Make static deployment for Herok

--- granbodagard/settings.py
+++ granbodagard/settings.py
@@ -179,6 +179,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = "static/css/"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
